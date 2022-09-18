1db918d2dde4ed7453c8658a7c296b9890e80a0c
Adilio89 <88595417+Adilio89@users.noreply.github.com>
2022-09-18 14:20:22 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3._1_VolumeDeUmaCaixa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double comprimento, altura, largura, volume;
+            Console.WriteLine("Calculo do Volume De Uma Caixa:");
+            Console.Write("Digite o comprimento: ");
+            comprimento = double.Parse(Console.ReadLine());
+            Console.Write("Digite a altura: ");
+            altura = double.Parse(Console.ReadLine());
+            Console.Write("Digite a largura: ");
+            largura = double.Parse(Console.ReadLine());
+            volume = comprimento * altura * largura;
+            Console.Write("O Volume da sua caixa é: " + volume + "cm³");
+            Console.ReadKey();
+        }
+    }
+}
